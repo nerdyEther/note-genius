2ece4f8f1690f85944eb79530490f4287f490864
nerdyEther <ether1926@gmail.com>
2024-11-13 05:01:20 +0530

    solving security issue

--- client/firebase-admin.ts
+++ client/firebase-admin.ts
@@ -1,31 +1,29 @@
-import { 
-    initializeApp, 
-    getApps, 
-    App, 
-    getApp, 
-    cert 
-} from "firebase-admin/app";
-
+import { initializeApp, getApps, App, getApp, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 
-const serviceKey= require("@/service_key.json")
+const serviceAccount = {
+    type: "service_account",
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: "https://accounts.google.com/o/oauth2/auth",
+    token_uri: "https://oauth2.googleapis.com/token",
+    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL
+};
 
-let app:App;
+let app: App;
 
-if(getApps().length === 0)
-{
-    app=initializeApp({
-            credential: cert(serviceKey),
-
+if (getApps().length === 0) {
+    app = initializeApp({
+        credential: cert(serviceAccount as any),
     });
-
-}
-else
-{
-    app= getApp();
+} else {
+    app = getApp();
 }
 
+const adminDb = getFirestore(app);
 
-const adminDb= getFirestore(app);
-
-export {app as adminApp, adminDb};
+export { app as adminApp, adminDb };
--- client/firebase.ts
+++ client/firebase.ts
@@ -1,17 +1,16 @@
-    import {initializeApp, getApps, getApp} from "firebase/app"
-    import {getFirestore} from "firebase/firestore";
+import { initializeApp, getApps, getApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore";
 
-    const firebaseConfig = {
-        apiKey: "AIzaSyBjhm-dQ47g7IK7wnhZX9qL9-xXvaSewE0",
-        authDomain: "note-genius-1bdd7.firebaseapp.com",
-        projectId: "note-genius-1bdd7",
-        storageBucket: "note-genius-1bdd7.appspot.com",
-        messagingSenderId: "103420619977",
-        appId: "1:103420619977:web:e91199592415b7fd44f118"
-      };
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
+};
 
-      const app= getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore(app);
 
-      const db = getFirestore(app);
-
-      export{db};
+export { db };
