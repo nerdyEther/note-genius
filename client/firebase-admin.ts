070b72fc9e7fbca3712e2955e8f9105b34f29de1
nerdyEther <ether1926@gmail.com>
2024-11-13 08:28:43 +0530

    resolving firebase issue

--- client/firebase-admin.ts
+++ client/firebase-admin.ts
@@ -11,7 +11,8 @@ const serviceAccount = {
     auth_uri: "https://accounts.google.com/o/oauth2/auth",
     token_uri: "https://oauth2.googleapis.com/token",
     auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+    universe_domain: "googleapis.com"
 };
 
 let app: App;
